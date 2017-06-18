4032d174127a69880cf5e538d4074ba25370f663
Qun <slvt@qq.com>
2017-06-18 15:24:46 +0800

    HomeFragment布局

--- app/src/main/java/com/qun/takeout/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/qun/takeout/ui/fragment/HomeFragment.java
@@ -10,6 +10,15 @@ import android.widget.TextView;
 import com.qun.takeout.R;
 
 /**
+ * 工作内容：
+ * 1、布局
+ * 2、头容器的处理
+ * a.需要侵入到状态栏中
+ * b.状态栏为透明
+ * c.随着RecyclerView的滑动，头的透明度会变动
+ * 3、RecyclerView数据加载
+ * a.简单数据加载
+ * b.复杂数据加载
  * Created by Qun on 2017/6/18.
  */
 
@@ -18,7 +27,7 @@ public class HomeFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment, null);
+        return inflater.inflate(R.layout.fragment_home, null);
     }
 
     @Override
